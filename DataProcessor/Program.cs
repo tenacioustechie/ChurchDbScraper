543e975098c42e1c735d25545b04a54a4ef585ef
Brian Mills <brian@millsit.com>
2024-08-11 10:30:36 +1000

    adding stream json reader

--- DataProcessor/Program.cs
+++ DataProcessor/Program.cs
@@ -1,9 +1,15 @@
-﻿using System.Text.Json;
+﻿using DataProcessor.models;
+using System.Text.Json;
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+var path = Path.Combine(System.IO.Directory.GetCurrentDirectory(), "..\\..\\..\\acncCharityDataRaw.json");
+Console.WriteLine( path);
+// D:\codeme\ChurchDbScraper\DataProcessor\bin\Debug\net8.0
 
-JsonSerializer.Deserialize(Accepts a Stream)
+var stream = new StreamReader(path);
+var reader = JsonSerializer.Deserialize<List<CharityRaw>>(stream);
+//Console.WriteLine(reader.)
 
 
